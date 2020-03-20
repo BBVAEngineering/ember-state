2e06ab87acb283d01b642e719019e2099e4c80b4
adrigzr <adrigzr@users.noreply.github.com>
2020-03-20 12:55:50 +0100

    style: remove commented code

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,8 +1,6 @@
 import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
-// import './helpers/back';
-// import './helpers/replace';
 import { start } from 'ember-qunit';
 
 setApplication(Application.create(config.APP));
